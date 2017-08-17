9d926d34aca609013dc80be6947a88c340e60bb6
Your Name Here <mariamabubakar@icloud.com>
2017-08-18 10:05:20 +1200

    added back to top button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+
+
+
+// ------- Hide Logo Tab -------- 
+
 var prev = 0;
 var $window = $(window);
 var nav = $('.nav');
@@ -6,4 +11,24 @@ $window.on('scroll', function(){
   var scrollTop = $window.scrollTop();
   nav.toggleClass('hidden', scrollTop > prev);
   prev = scrollTop;
+});
+
+
+
+
+
+
+// -------- Scroll to Top --------
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 500) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(0);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(0);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
 });
