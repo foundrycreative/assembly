985a27aaa8108e7f77449a65f73791f8c5f03469
Your Name Here <mariamabubakar@icloud.com>
2017-08-18 14:19:37 +1200

    rearranged files to allow for two bm animations. added new coffees ubscription animation

--- js/bm.js
+++ js/bm.js
@@ -1,7 +1,15 @@
 var animation = bodymovin.loadAnimation({
-	container: document.getElementById('bm'),
+	container: document.getElementById('bm-1'),
 	renderer: 'svg',
 	loop: true,
 	autoplay: true,
-	path: 'data.json',
+	path: 'anim/header-illu-anim/data.json',
+});
+
+var animation = bodymovin.loadAnimation({
+	container: document.getElementById('bm-2'),
+	renderer: 'svg',
+	loop: true,
+	autoplay: true,
+	path: 'anim/coffee-sub-anim/data.json',
 });
