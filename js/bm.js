cf5a1f0fb358ddf56cc1e8319ab45eafe7d3a52a
Your Name Here <mariamabubakar@icloud.com>
2017-08-18 14:26:15 +1200

    testing anim

--- js/bm.js
+++ js/bm.js
@@ -6,10 +6,10 @@ var animation = bodymovin.loadAnimation({
 	path: 'anim/header-illu-anim/data.json',
 });
 
-var animation = bodymovin.loadAnimation({
-	container: document.getElementById('bm-2'),
-	renderer: 'svg',
-	loop: true,
-	autoplay: true,
-	path: 'anim/coffee-sub-anim/data.json',
-});
+// var animation = bodymovin.loadAnimation({
+// 	container: document.getElementById('bm-2'),
+// 	renderer: 'svg',
+// 	loop: true,
+// 	autoplay: true,
+// 	path: 'anim/coffee-sub-anim/data.json',
+// });
