7879ed10cad931159f2c27db107c57e92dd9f02d
Your Name Here <mariamabubakar@icloud.com>
2017-10-04 14:09:33 +1300

    removed animation from logo tab at top

--- js/index.js
+++ js/index.js
@@ -3,15 +3,15 @@
 
 // ------- Hide Logo Tab -------- 
 
-var prev = 0;
-var $window = $(window);
-var nav = $('.nav');
+// var prev = 0;
+// var $window = $(window);
+// var nav = $('.nav');
 
-$window.on('scroll', function(){
-  var scrollTop = $window.scrollTop();
-  nav.toggleClass('hidden', scrollTop > prev);
-  prev = scrollTop;
-});
+// $window.on('scroll', function(){
+//   var scrollTop = $window.scrollTop();
+//   nav.toggleClass('hidden', scrollTop > prev);
+//   prev = scrollTop;
+// });
 
 
 
